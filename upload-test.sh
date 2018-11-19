6896fcc17086f8f1bf4071db83fce023c46163cc
joyongjin <wnrhd114@gmail.com>
2018-11-19 13:18:03 +0900

    script

--- upload-test.sh
+++ upload-test.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
-if [ -d build ]; then rm -r build
-fi
+clear()
+{
+    if [[ -d build ]]; then rm -r build
+    fi
 
-if [ -d dist ]; then rm -r dist
-fi
+    if [[ -d dist ]]; then rm -r dist
+    fi
 
-if [ -d django_jackal.egg-info ]; then rm -r django_jackal.egg-info
-fi
+    if [[ -d django_jackal.egg-info ]]; then rm -r django_jackal.egg-info
+    fi
+}
 
+clear
 python setup.py sdist bdist_wheel
 twine upload -r pypitest dist/*
+clear
